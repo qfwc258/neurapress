c2e18cc7b27fbe6a77009c52527557a338fe68c1
chenw <29137819+qfwc258@users.noreply.github.com>
2025-05-14 08:05:18 +0800

    Update types.ts

--- src/lib/markdown/types.ts
+++ src/lib/markdown/types.ts
@@ -114,7 +114,7 @@ export interface RendererOptions {
     mermaid?: StyleOptions
   }
   inline?: {
-    strong?: StyleOptions
+    strong?: string
     em?: StyleOptions
     codespan?: StyleOptions
     link?: StyleOptions
